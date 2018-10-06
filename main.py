707918e6cbbe8d5fa47ccf7ec7a968d6956893f9
Maestorme <maestorme@gmail.com>
2018-10-06 11:33:22 -0400

    HangmanAI: Function to get frequency of letters in corpus.
    Go through all the words in the english dictionary and find the frequency
    of letters in those words

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import nltk
+from nltk.corpus    import words
+from string         import ascii_uppercase
+import requests
+import time
+
+
+def get_freq_list():
+    letter_to_frequency = {letter: 0 for letter in ascii_uppercase}
+    for word in words.words():
+        for character in word:
+            try:
+                letter_to_frequency[character.upper()] += 1
+            except KeyError:
+                continue
+    sorted_by_value = sorted(letter_to_frequency.items(), key=lambda kv: kv[1], reverse=True)
+    return ''.join([letter for letter,frequency in sorted_by_value])
+
+
 if __name__ == "__main__":
-    print("Hello")
+    print(get_freq_list())
+
